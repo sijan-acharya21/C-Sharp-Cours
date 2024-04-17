8945823522f4dc99813e815c3ce52f08d60311cb
sijan-acharya21 <136575897+sijan-acharya21@users.noreply.github.com>
2024-04-17 12:28:28 +1000

    Create declaring_variables.cs

---
+++ declaring_variables.cs
@@ -0,0 +1,7 @@
+// Declaring variables
+int myInt = 5; // Initializing a variable because the value is already there initially
+
+// Declaring multiple variables at once
+int myInt2, myInt3;
+
+iny  myInt4 = 4, myInt5 = 5; // Only one data type and one semicolon
