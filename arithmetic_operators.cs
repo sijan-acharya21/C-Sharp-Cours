0ebccb9f2b3e090367c7081e05bba4827e8cbc73
sijan-acharya21 <136575897+sijan-acharya21@users.noreply.github.com>
2024-04-28 21:51:23 +1000

    Update arithmetic_operators.cs

--- arithmetic_operators.cs
+++ arithmetic_operators.cs
@@ -16,3 +16,21 @@ int operation = 2 + 4 * 3; // 14 => Multiplication is higher precedence (4 * 3) 
 // Parentheses can change the operation
 int operation2 = (2 + 4) * 3; // 18 => Parentheses is highest in precedence (6) * 3
 
+// Different ways to incrementing variables
+int x = 5;
+x = x + 1; // First way of incrementing
+x += 1; // Second way of incrementing
+
+// Incrementing variables using operator as prefix and postfix
+
+// Prefix operator
+int c = 5;
+Console.WriteLine(c);
+int d = c++;
+Console.WriteLine("Value of c: {0} and value of d: {1}", c, d);
+
+// Postfix operator
+int a = 10;
+Console.WriteLine(a);
+int b = a++;
+Console.WriteLine("Value of a: {0} and value of b: {1}", a, b); // a = 11, b = 10
