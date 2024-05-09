0474f80866a33bbcefbd82ae6b05a6d9fea4f25a
sijan-acharya21 <136575897+sijan-acharya21@users.noreply.github.com>
2024-05-09 16:05:19 +1000

    Update using_strings.cs

--- using_strings.cs
+++ using_strings.cs
@@ -10,10 +10,23 @@ Console.WriteLine("Names {0} and {1} are equal:  {2}", name1, name2, namesEqual)
 string name3 = "Denzel";
 string name4 = "Samuel";
 
+// string.Equals() function returns true if both strings are the same and false when they are different
+// Place the two strings inside the parentheses: string.Equals(string1, string2)
 bool areNamesEqual = string.Equals(name3, name4);
 Console.WriteLine("{0} and {1} are equal: {2}", name3, name4, areNamesEqual);
 
-// String interpolation - substitute values of variables as placeholders in strings
+// Not from video: String interpolation - substitute values of variables as placeholders in strings
 string artist = "The Weeknd";
 // Use the $ before the double quotes and place the variables inside the curly braces
 Console.WriteLine($"The artist's name is {artist}");
+
+// string.Compare() function returns an integer
+// Place the two strings being compared inside the parentheses: string.Compare(string1, string2)
+// Returns -1 if the first string is less than the second string in alphabetical order
+// Returns 0 if both strings are the same
+// Returns 1 if first string is greater than the second string in alphabetical order
+string singer1 = "Abel Tesfaye";
+string singer2 = "Micheal Jackson";
+int compareString = string.Compare(singer1, singer2);
+Console.WriteLine(compareString);
+
