432979a89a80cc1faf472574e7e17b36294f9786
sijan-acharya21 <136575897+sijan-acharya21@users.noreply.github.com>
2024-05-09 15:52:23 +1000

    Update using_strings.cs

--- using_strings.cs
+++ using_strings.cs
@@ -1,3 +1,19 @@
 string myString = "Sijan \" Acharya"; // Use \ to escape double quotes
 Console.WriteLine(myString); // Sijan \" Acharya
 
+string name1 = "Sijan";
+string name2 = "Sijan";
+
+bool namesEqual = name1 == name2;
+Console.WriteLine("Names {0} and {1} are equal:  {2}", name1, name2, namesEqual);
+
+string name3 = "Denzel";
+string name4 = "Samuel";
+
+bool areNamesEqual = string.Equals(name3, name4);
+Console.WriteLine("{0} and {1} are equal: {2}", name3, name4, areNamesEqual);
+
+// String interpolation - substitute values of variables as placeholders in strings
+string artist = "The Weeknd";
+// Use the $ before the double quotes and place the variables inside the curly braces
+Console.WriteLine($"The artist's name is {artist}");
