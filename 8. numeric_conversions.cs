bcc191e8cc48394667a1d658df3d2ff27a2b007d
sijan-acharya21 <136575897+sijan-acharya21@users.noreply.github.com>
2024-05-11 09:37:44 +1000

    Update 8. numeric_conversions.cs

--- 8. numeric_conversions.cs
+++ 8. numeric_conversions.cs
@@ -1,1 +1,11 @@
-// Im
+// Implicit casting - automatic conversion 
+int x = 3;
+double y = x;
+// Conversion does not have any loss of data
+Console.WriteLine(y); // A double can take a whole number and decimal points
+
+// Explicit casting - clearly telling the compiler to convert data types
+double a = 5.5;
+// Put the data type that you want to convert to inside parentheses next to the variable
+int b = (int) a; // Telling the compiler I am going to lose data
+Console.WriteLine(b);
