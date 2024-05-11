9d0573a62c77a6fe776afb17c035383fe4681fa3
sijan-acharya21 <136575897+sijan-acharya21@users.noreply.github.com>
2024-05-11 13:18:18 +1000

    Update 9. declaring_constants.cs`

--- 9. declaring_constants.cs
+++ 9. declaring_constants.cs
@@ -1,1 +1,19 @@
+// The value of a constant cannot be changed after declaring
+const double myConstant = 5;
 
+// Accessing pi constant
+const double PI = Math.PI;
+Console.WriteLine(PI);
+
+// Enumeration are a group of constants
+enum DaysOfWeek {
+  SUNDAY,
+  MONDAY,
+  TUESDAY,
+  WEDNESDAY,
+  THURSDAY,
+  FRIDAY,
+  SATURDAY
+}
+
+DaysOfWeek today = DaysOfWeek.SATURDAY;
