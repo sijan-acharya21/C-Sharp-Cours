194b225d65d0e8821f768bd707b46680d986e717
sijan-acharya21 <136575897+sijan-acharya21@users.noreply.github.com>
2024-05-12 20:23:27 +1000

    Update 9. declaring_constants.cs

--- 9. declaring_constants.cs
+++ 9. declaring_constants.cs
@@ -5,15 +5,21 @@ const double myConstant = 5;
 const double PI = Math.PI;
 Console.WriteLine(PI);
 
-// Enumeration are a group of constants
-enum DaysOfWeek {
-  SUNDAY,
-  MONDAY,
+// Enumeration is the action of mentioning a number of things one by one
+// Can also set the index for the enums: SUNDAY = 1, default index is 0
+enum Day {
+  // Index starts at 0 at default
+  MONDAY, // Can also set index MONDAY = 1,
   TUESDAY,
   WEDNESDAY,
   THURSDAY,
   FRIDAY,
-  SATURDAY
+  SATURDAY,
+  SUNDAY
 }
 
-DaysOfWeek today = DaysOfWeek.SATURDAY;
+Day tomorrow = Day.Monday;
+Console.WriteLine("Tomorrow is {0}", tomorrow);
+
+int tomorrowIndex = (int)tomorrow;
+Console.WriteLine("Tomorrow is day {0}", tomorrowIndex);
