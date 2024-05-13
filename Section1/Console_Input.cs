122a8eb0fb16ccbbcdb298c0d3295dfb6fd960cb
sijan-acharya21 <136575897+sijan-acharya21@users.noreply.github.com>
2024-05-13 16:14:41 +1000

    Update Console_Input.cs

--- Section1/Console_Input.cs
+++ Section1/Console_Input.cs
@@ -1,8 +1,14 @@
 // When program is run it closes immendiately because it is not waiting for any input
-Console.WriteLine("What is your name?");
+Console.WriteLine("This is user input");
 
 // Console.ReadLine() is always written in as a string
 // Console.ReadLine() is waiting at the console for the user to press Enter
-string name = Console.ReadLine();
+Console.WriteLine("What is your first name?"); 
 
-Console.WriteLine("Hello, {0}", name);
+string firstName = Console.ReadLine();
+
+Console.WriteLine("What is your last name?");
+
+string lastName = Console.ReadLine();
+
+Console.WriteLine($"Hello, {firstName} {lastName}");
