2798b6534cae9a545a323492c22c5c7b20145acc
sijan-acharya21 <136575897+sijan-acharya21@users.noreply.github.com>
2024-05-13 16:19:19 +1000

    Update Console_Input.cs

--- Section1/Console_Input.cs
+++ Section1/Console_Input.cs
@@ -1,5 +1,5 @@
 // When program is run it closes immendiately because it is not waiting for any input
-Console.WriteLine("What is your age?00");
+Console.WriteLine("What is your age?");
 
 // Console.ReadLine() is always written in as a string
 // Console.ReadLine() is waiting at the console for the user to press Enter
