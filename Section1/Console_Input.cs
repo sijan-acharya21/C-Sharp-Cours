055e1d7754098d8dabe3dc70245297e2720d95c0
sijan-acharya21 <136575897+sijan-acharya21@users.noreply.github.com>
2024-05-15 12:42:02 +1000

    Update Console_Input.cs

--- Section1/Console_Input.cs
+++ Section1/Console_Input.cs
@@ -6,9 +6,13 @@ Console.WriteLine("What is your age?");
 Console.WriteLine("What is your first name?"); 
 
 string firstName = Console.ReadLine();
-
 Console.WriteLine("What is your last name?");
-
 string lastName = Console.ReadLine();
-
 Console.WriteLine($"Hello, {firstName} {lastName}");
+
+// Parsing is converting a string to a numeric value
+Console.WriteLine("Enter a number");
+// Parsing the value being written by the user because Console.ReadLine() is written as a string
+int number = int.Parse(Console.ReadLine());
+number += 10;
+Console.WriteLine(number)
