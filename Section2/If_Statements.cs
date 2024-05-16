856ad2c9d46d804577dfbf9d24eaba243a10b3e5
sijan-acharya21 <136575897+sijan-acharya21@users.noreply.github.com>
2024-05-17 00:49:45 +1000

    Update If_Statements.cs

--- Section2/If_Statements.cs
+++ Section2/If_Statements.cs
@@ -11,9 +11,24 @@ if (age > 18)
     Console.WriteLine("Age is greater than 18"); // By default only the first statement after the if statement is considered part of it
     Console.WriteLine("You can attend the party"); // This statement is considered outside the if statement so it won't run
 
-// Both statements are included inside the if statement because of curly braces (blocks
+// Both statements are included inside the if statement because of curly braces (blocks)
 age = 21;
+// To execute multiple statements inside the if statement use blocks
 if (age > 18) {
     Console.WriteLine("Age is greater than 18"); 
     Console.WriteLine("You can attend the party");
 }
+
+bool isSoftwareEngineer = true;
+// By default isSoftwareEngineer is a true or false value so it will run the statement below
+if (isSoftwareEngineer) {
+    Console.WriteLine("Person is a software engineer");
+}
+
+isSoftwareEngineer = false;
+// Won't run the statement because value is not true
+if (isSoftwareEngineer) { // Same as (isSoftwareEngineer == true)
+    Console.WriteLine("Person is a software engineer");
+}
+// Runs the statement below if isSoftwareEngineer == false
+Console.WriteLine("Person is NOT a software engineer"); // Same as (isSoftwareEngineer == true)
