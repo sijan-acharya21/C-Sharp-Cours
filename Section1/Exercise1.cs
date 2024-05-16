ddd0763ebb0aa506ac7149824dd75737714e3700
sijan-acharya21 <136575897+sijan-acharya21@users.noreply.github.com>
2024-05-16 13:39:22 +1000

    Update Exercise1.cs

--- Section1/Exercise1.cs
+++ Section1/Exercise1.cs
@@ -1,1 +1,9 @@
+// Build a simple calculator. Read in two integers from end user and add them together. Print the results in a nice output statement
 
+Console.WriteLine("Enter your first integer");
+int integer1 = int.Parse(Console.ReadLine());
+Console.WriteLine("Enter your second integer");
+int integer2 = int.Parse(Console.ReadLine());
+int sum = integer1 + integer2;
+
+Console.WriteLine($"{integer1} and {integer2} add up to {sum}");
