40a25ed9130b927cc34ef85edbfe526597b9b301
sijan-acharya21 <136575897+sijan-acharya21@users.noreply.github.com>
2024-05-18 11:54:57 +1000

    Update If_Else_Statements.cs

--- Section2/If_Else_Statements.cs
+++ Section2/If_Else_Statements.cs
@@ -1,6 +1,20 @@
-bool isSoftwareEngineer = true;
-if (isSoftwareEngineer) {
-  Console.WriteLine("This is the true part");
-} else {
-  Console.WriteLine("This is the false part");
+int age = 50;
+// If the boolean below is true then it runs the if block
+if (age < 18)
+{
+    Console.WriteLine("This is the true");
+} else // If the boolean above is false then it runs the else block
+{
+    Console.WriteLine("This is the false");
+}
+
+// Age restriction program
+Console.WriteLine("Enter your age");
+int age = int.Parse(Console.ReadLine());
+if (age >= 18)
+{
+    Console.WriteLine("You're good to go");
+} else
+{
+    Console.WriteLine("Sorry, you aren't 18 yet");
 }
