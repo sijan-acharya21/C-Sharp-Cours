3ea449e7f17b491e56e3b2f2cd4dcf54a79cbec2
sijan-acharya21 <136575897+sijan-acharya21@users.noreply.github.com>
2024-05-18 16:56:27 +1000

    Update Compound_Expressions.cs

--- Section2/Compound_Expressions.cs
+++ Section2/Compound_Expressions.cs
@@ -13,9 +13,9 @@ bool admissionToSchool = athleteScholarship || perfectGrades;
 Console.WriteLine(admissionToSchool);
 
 // Another age restriction program
-Console.WriteLine("Enter you age");
+Console.WriteLine("Enter your age");
 int age = int.Parse(Console.ReadLine());
-
+// Checks if an individual is eligible for the party
 if (age >= 13 && age <= 19)
 {
     Console.WriteLine("Allowed in the teenage party");
@@ -23,3 +23,44 @@ if (age >= 13 && age <= 19)
 {
     Console.WriteLine("Sorry, this is a teenage party");
 }
+
+// Movie discount
+
+if (age < 12 || (age > 65 && movieRating.ToUpper() == "G"))
+{
+    Console.WriteLine("You get a discount");
+} else
+{
+    Console.WriteLine("You DONT get a discount");
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
