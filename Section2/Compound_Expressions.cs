7315302cca90df82c8a2c312e588664731aa4c0e
sijan-acharya21 <136575897+sijan-acharya21@users.noreply.github.com>
2024-05-18 14:45:08 +1000

    Update Compound_Expressions.cs

--- Section2/Compound_Expressions.cs
+++ Section2/Compound_Expressions.cs
@@ -1,1 +1,16 @@
+// AND operator (&&) combines two conditions together
 
+bool x = true;
+bool y = true;
+
+bool z = x && y;
+Console.WriteLine(z);
+
+/* 
+AND operator is true when all conditions are met
+
+True && True == True
+True && False == False
+False && True == False
+False && False == False
+*/
