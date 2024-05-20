8fc70327c953ceac17450cd860aba84785ff9acc
sijan-acharya21 <136575897+sijan-acharya21@users.noreply.github.com>
2024-05-20 21:11:31 +1000

    Create Exercise3.cs

---
+++ Section2/Exercise/Exercise3.cs
@@ -0,0 +1,24 @@
+/*
+Write a program that generates a random number between 1 and 10.
+Ask a use to guess the random number, then display the random number and a message indicating whether the user's guess was too high, too low or correct
+
+Hint:
+You can create a random number that is at least min but less than max using the following statements
+Random randomGenerator = new Random();
+int randomNumber = randomGenerator.Next(min, max);
+*/
+
+Random randomGenerator = new Random();
+int randomNumber = randomGenerator.Next(min, max);
+Console.WriteLine(randomGenerator)
+
+Console.WriteLine("Guess the random number");
+int guess = int.Parse(Console.ReadLine());
+
+if (guess < randomNumber) {
+  Console.WriteLine($"Your guess {guess} was less than the random number {randomNumber}");
+} else if (guess == randomNumber) {
+  Console.WriteLine("Correct, you guessed the random number {randomNumber}");
+} else if (guess > randomNumber) {
+  Console.WriteLine("Your guess {guess} was greater than the random number {randomNumber}");
+}
