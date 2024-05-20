25c6d29bd0608e72e166b0caf3c21c61e03e05f0
sijan-acharya21 <136575897+sijan-acharya21@users.noreply.github.com>
2024-05-21 00:19:16 +1000

    Update Exercise3.cs

--- Section2/Exercise/Exercise3.cs
+++ Section2/Exercise/Exercise3.cs
@@ -8,16 +8,18 @@ Random randomGenerator = new Random();
 int randomNumber = randomGenerator.Next(min, max);
 */
 
+// Instantiate random number generator
 Random randomGenerator = new Random();
-int randomNumber = randomGenerator.Next(1, 10);
+// Random.Next() returns a non-negative random integer
+int randomNumber = randomGenerator.Next(1, 11);
 
 Console.WriteLine("Guess the random number");
 int guess = int.Parse(Console.ReadLine());
 
 if (guess < randomNumber) {
-  Console.WriteLine($"Your guess {guess} was less than the random number {randomNumber}");
+  Console.WriteLine($"Your guess {guess} is LESS THAN {randomNumber}");
 } else if (guess == randomNumber) {
-  Console.WriteLine($"Correct, you guessed the random number {randomNumber}");
+  Console.WriteLine($"Your guess {guess} is CORRECT");
 } else if (guess > randomNumber) {
-  Console.WriteLine($"Your guess {guess} was greater than the random number {randomNumber}");
+  Console.WriteLine($"Your guess {guess} is GREATER THAN {randomNumber}");
 }
