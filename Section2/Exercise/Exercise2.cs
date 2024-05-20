84014f565d381cb1c44b2a1fdc34f9f1b871dea4
sijan-acharya21 <136575897+sijan-acharya21@users.noreply.github.com>
2024-05-20 20:44:04 +1000

    Update Exercise2.cs

--- Section2/Exercise/Exercise2.cs
+++ Section2/Exercise/Exercise2.cs
@@ -17,11 +17,11 @@ if (area < 400) {
   cost = 25;
   Console.WriteLine($"Weekly cost is {cost * area}");
   Console.WriteLine($"Seasonal cost is ${cost * area * seasonWeeks}");
-} else if (area > 400 && area < 600) {
+} else if (area >= 400 && area < 600) {
   cost = 35;
   Console.WriteLine($"Weekly cost is {cost * area}");
   Console.WriteLine($"Seasonal cost is ${cost * area * seasonWeeks}");
-} else if (area > 600) {
+} else if (area >= 600) {
   cost = 50;
   Console.WriteLine($"Weekly cost is {cost * area}");
   Console.WriteLine($"Seasonal cost is ${cost * area * seasonWeeks}");
