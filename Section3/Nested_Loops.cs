59dc832b3647ea51e71941872587ccb2b42553ca
sijan-acharya21 <136575897+sijan-acharya21@users.noreply.github.com>
2024-05-23 00:31:00 +1000

    Update Nested_Loops.cs

--- Section3/Nested_Loops.cs
+++ Section3/Nested_Loops.cs
@@ -1,1 +1,37 @@
 // Nested means one component inside another component
+// A nested loop is a loop within another loop
+for (int i = 0; i < 10; i++) {
+  Console.Write("*");
+}
+
+// Creating a square
+for (int i = 0; i < 10; i++)
+{
+    Console.Write("*");
+}
+Console.WriteLine();
+for (int i = 0; i < 10; i++)
+{
+    Console.Write("*");
+}
+Console.WriteLine();
+for (int i = 0; i < 10; i++)
+{
+    Console.Write("*");
+}
+Console.WriteLine();
+for (int i = 0; i < 10; i++)
+{
+    Console.Write("*");
+}
+Console.WriteLine();
+for (int i = 0; i < 10; i++)
+{
+    Console.Write("*");
+}
+Console.WriteLine();
+for (int i = 0; i < 10; i++)
+{
+    Console.Write("*");
+}
+Console.WriteLine();
