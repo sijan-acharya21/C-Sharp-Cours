4e261b3f8e640050761bd383134de07958a4aa80
sijan-acharya21 <136575897+sijan-acharya21@users.noreply.github.com>
2024-05-22 23:55:37 +1000

    Update Do_While_Loops.cs

--- Section3/Do_While_Loops.cs
+++ Section3/Do_While_Loops.cs
@@ -1,1 +1,14 @@
+// Do while loop will execute the body at least once whereas while loop does not
 
+// While loop syntax
+int x = 5;
+while (x > 6) { // While loop does not run because 
+  Console.WriteLine("Hello");
+}
+
+// Do while loop syntax
+int i = 5;
+do {
+  Console.WriteLine("Hello");
+  ;
+} while (i > 6);
