b2bd7592a62403aec2e8b491d0c79cdd042a3cea
sijan-acharya21 <136575897+sijan-acharya21@users.noreply.github.com>
2024-05-23 01:18:08 +1000

    Update Exercise1.cs

--- Section3/Exercise/Exercise1.cs
+++ Section3/Exercise/Exercise1.cs
@@ -1,1 +1,13 @@
+/*
+Write a console-based application that prompts a user for an hourly pay rate.
+While the user enter the values less than $5.65 or greater than $49.99, continue to prompt the user.
+Before the program ends, display the valid pay rate.
+*/
 
+Console.WriteLine("Enter an hourly pay rate");
+double hourlyRate = int.Parse(Console.ReadLine());
+
+while (hourlyRate < 5.65 || hourlyRate > 49.99) {
+  Console.WriteLine("Enter an hourly pay rate");
+}
+Console.WriteLine("The valid pay rate is ${0}", hourlyRate);
