d4c5cd38e911fb222971b382499e5dfc78e838e7
sijan-acharya21 <136575897+sijan-acharya21@users.noreply.github.com>
2024-05-23 02:13:31 +1000

    Update Exercise3.cs

--- Section3/Exercise/Exercise3.cs
+++ Section3/Exercise/Exercise3.cs
@@ -1,1 +1,19 @@
+/*
+EDIT TO USE LOOPS
 
+Write a program that generates a random number between 1 and 10.
+Ask a user to guess the random number, then display the random number and a message indicating whether the user's guess was too high, too low or correct.
+
+You can create a random number that is at least min but less than max using the following statements
+Random randomGenerator = new Random();
+int randomNumber = randomGenerator.Next(min, max); 1, 11
+*/
+
+// Generate a random number between 1 and 10
+Random randomGenerator = new Random();
+int randomNumber = randomGenerator.Next(1, 11);
+
+Console.WriteLine("Enter your guess");
+int guess = int.Parse(Console.ReadLine());
+
+// Using loops to guess again rather than giving one chance for the user
