e5110f3d57d7ee5395daf98919f66ba7327622a6
sijan-acharya21 <136575897+sijan-acharya21@users.noreply.github.com>
2024-05-22 23:20:31 +1000

    Update For_Loops.cs

--- Section3/For_Loops.cs
+++ Section3/For_Loops.cs
@@ -10,8 +10,9 @@ for (int i = 0; i < 5; i++) { // For loop has all the components inside the pare
  Console.WriteLine("Hello");
 }
 
+// Printing hello based on the user input
 Console.WriteLine("Enter an amount of times to print hello");
-int timesPrint = int.Parse(Console.ReadLine());
-for (int i = 0; i < timesPrint; i++) {
+int amount = int.Parse(Console.ReadLine());
+for (int i = 0; i < amount; i++) {
  Console.WriteLine("Hello");
 }
