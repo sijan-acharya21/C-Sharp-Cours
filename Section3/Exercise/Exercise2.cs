9878c83f6a595aa04d3dbe2fe2a55f17e48849da
sijan-acharya21 <136575897+sijan-acharya21@users.noreply.github.com>
2024-05-23 01:57:20 +1000

    Update Exercise2.cs

--- Section3/Exercise/Exercise2.cs
+++ Section3/Exercise/Exercise2.cs
@@ -1,1 +1,15 @@
+/*
+Write a console-based application that sums the integers from 1 to 50
+*/
 
+int sum = 0;
+Console.WriteLine("Enter an integer");
+int integer = int.Parse(Console.ReadLine()); // 8
+
+while (integer >= 1 && integer <= 50) {
+  sum += integer;
+  Console.WriteLine("Enter an integer");
+  integer = int.Parse(Console.ReadLine()); // 10
+  
+}
+Console.WriteLine("Sum is {0}", sum);
