35076ec849d6c85d6b6213e4215b9be45f59fd32
sijan-acharya21 <136575897+sijan-acharya21@users.noreply.github.com>
2024-05-22 23:13:21 +1000

    Update For_Loops.cs

--- Section3/For_Loops.cs
+++ Section3/For_Loops.cs
@@ -1,14 +1,11 @@
-// For loop is useful for definite loops
+// While loop syntax
 int x = 0;
 while (x < 5) {
  Console.WriteLine("Hello");
  x++;
 }
 
-// For loop has all the components inside the parentheses
-for (int i = 0; i < 5; i++) {
+// For loop syntax
+for (int i = 0; i < 5; i++) { // For loop has all the components inside the parentheses
  Console.WriteLine("Hello");
 }
-
-
-
