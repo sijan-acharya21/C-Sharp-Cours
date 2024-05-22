f30b3a2049a19b69246ab767b93db93d68747941
sijan-acharya21 <136575897+sijan-acharya21@users.noreply.github.com>
2024-05-22 23:16:58 +1000

    Update For_Loops.cs

--- Section3/For_Loops.cs
+++ Section3/For_Loops.cs
@@ -9,3 +9,9 @@ while (x < 5) {
 for (int i = 0; i < 5; i++) { // For loop has all the components inside the parentheses
  Console.WriteLine("Hello");
 }
+
+Console.WriteLine("Enter an amount of times to print hello");
+int timesPrint = int.Parse(Console.ReadLine());
+for (int i = 0; i < timesPrint; i++) {
+ Console.WriteLine("Hello");
+}
