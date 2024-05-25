d89132084d82be1cdc25b2b72af0e608733c3f21
sijan-acharya21 <136575897+sijan-acharya21@users.noreply.github.com>
2024-05-26 00:40:27 +1000

    Update Array_Methods.cs

--- Section4/Array_Methods.cs
+++ Section4/Array_Methods.cs
@@ -1,1 +1,55 @@
+// Linear search or sequential search checks each element in the array
+// Binary search is more efficient than linear search - array must be in order before searching
+
+int[] myArray = { 10, 15, 20, 25, 30, 35, 40, 45 };
+Console.WriteLine("Enter number to check if it exists");
+int number = int.Parse(Console.ReadLine());
+
+/*
+Array.BinarySearch(arrayName, value)
+- Returns index of element is value is found
+- Returns -1 if value is not found
+*/
+
+int answer = Array.BinarySearch(myArray, number);
+
+if (answer < 0) {
+  Console.WriteLine("Number DOES NOT exist in the array");
+} else {
+  Console.WriteLine("{0} EXISTS in the array at index {1}", number, answer);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
