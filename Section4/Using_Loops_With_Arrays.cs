6fcba911fce77236e5eda718b332558585106c75
sijan-acharya21 <136575897+sijan-acharya21@users.noreply.github.com>
2024-05-25 19:33:42 +1000

    Update Using_Loops_With_Arrays.cs

--- Section4/Using_Loops_With_Arrays.cs
+++ Section4/Using_Loops_With_Arrays.cs
@@ -1,1 +1,8 @@
-
+// Using loops with arrays
+int[] grades = {66, 88, 95, 90, 100};
+// Subscript is the item number or the index
+Console.WriteLine(grades[0]) // How to say this (Grades subscript 0 OR Grades index 0)
+for (int i = 0; i < grades.Length; i++) {
+  grades[i] += 3;
+  Console.WriteLine(grades[i]);
+}
