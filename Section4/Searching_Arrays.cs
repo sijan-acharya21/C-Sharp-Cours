be98782a5e36d525733270b0d38a4bb5d243e4c5
sijan-acharya21 <136575897+sijan-acharya21@users.noreply.github.com>
2024-05-26 00:14:09 +1000

    Update Searching_Arrays.cs

--- Section4/Searching_Arrays.cs
+++ Section4/Searching_Arrays.cs
@@ -1,15 +1,22 @@
 string[] students = {"Bob", "Ted", "Tom", "Fred"};
+int[] grades = { 77, 50, 78, 99 };
 
 Console.WriteLine("Enter student name to search for");
 string studentName = Console.ReadLine();
-bool containsName = false;
+
+bool contains = false;
+int studentGrade = 0;
+
 for (int i = 0; i < students.Length; i++) {
   if (studentName == students[i]) {
-    contains == true;
+    contains = true;
+    studentGrade = grades[i];
+    break; // Break keyword will break out from the block
   }
 }
 if (contains == true) {
   Console.WriteLine("The student IS in class");
+  Console.WriteLine("The student grade is {0}", studentGrade);
 } else {
   Console.WriteLine("The student IS NOT in class");
 }
