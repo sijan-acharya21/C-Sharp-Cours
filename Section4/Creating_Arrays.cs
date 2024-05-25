121e33baac1dff10f08f922642f32e523826527a
sijan-acharya21 <136575897+sijan-acharya21@users.noreply.github.com>
2024-05-25 16:03:20 +1000

    Update Creating_Arrays.cs

--- Section4/Creating_Arrays.cs
+++ Section4/Creating_Arrays.cs
@@ -1,3 +1,17 @@
 // An array is a data strucure used for storing a group of the same data type
 
-// Creating an array
+// Syntax for creating an array
+// Datatype[] variableName = new Datatype[elements];
+string[] singers = new string[3];
+
+// An element is an individual item in the array
+// An index is the position of the element in the array
+// The first index has the value of 0 in the array
+
+singers[0] = "The Weeknd";
+singers[1] = "New Edition";
+singers[2] = "Michael Jackson";
+
+Console.WriteLine(singers[0]);
+Console.WriteLine(singers[1]);
+Console.WriteLine(singers[2]);
