958b1014e4d46b5f532c0cc17d532ddcc4f17c4e
sijan-acharya21 <136575897+sijan-acharya21@users.noreply.github.com>
2024-05-26 00:01:24 +1000

    Update Searching_Arrays.cs

--- Section4/Searching_Arrays.cs
+++ Section4/Searching_Arrays.cs
@@ -1,2 +1,15 @@
 string[] students = {"Bob", "Ted", "Tom", "Fred"};
 
+Console.WriteLine("Enter student name to search for");
+string studentName = Console.ReadLine();
+bool containsName = false;
+for (int i = 0; i < students.Length; i++) {
+  if (studentName == students[i]) {
+    contains == true;
+  }
+}
+if (contains == true) {
+  Console.WriteLine("The student IS in class");
+} else {
+  Console.WriteLine("The student IS NOT in class");
+}
