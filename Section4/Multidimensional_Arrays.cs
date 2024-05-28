57c5924695c1b4e9dcf48df0cd42d81e9dc03ab3
sijan-acharya21 <136575897+sijan-acharya21@users.noreply.github.com>
2024-05-28 21:44:46 +1000

    Update Multidimensional_Arrays.cs

--- Section4/Multidimensional_Arrays.cs
+++ Section4/Multidimensional_Arrays.cs
@@ -1,1 +1,9 @@
+// One dimensional array
+int[] myArray = { 5, 6, 7 };
 
+// There are two types of multidimensional arrays - rectangular and jagged arrays
+
+// Rectangular array is structured like a rectangular structure
+
+ 
+// 2.56 Video
