519faea543c29ab95ff3561eb50e53761f1b76fc
sijan-acharya21 <136575897+sijan-acharya21@users.noreply.github.com>
2024-06-01 15:53:58 +1000

    Update Exercise1.cs

--- Section4/Exercise/Exercise1.cs
+++ Section4/Exercise/Exercise1.cs
@@ -4,28 +4,31 @@ The program contains an array that holds the ten zip codes to which the company 
 Prompt a user to enter a zip code and display a message indicating whether the zip code is one which the company delivers.
 */
 
+// Prompt the user to enter a zip code
 Console.WriteLine("Enter your zip code for delivery");
-int zipCode = int.Parse(Console.ReadLine());
+// Store the zip code entered by the user
+int zipCode = int.Parse(Console.ReadLine()); 
 
+// Create the zipcodes which are available for delivery
 int[] eligibleZip = { 2200, 2210, 2220, 2230, 2240, 2250, 2260, 2270, 2280, 2290 };
-bool checkZip = true;
+// Create a boolean and set it to false for checking zip
+bool checkZip = false;
 
 for (int i = 0; i < eligibleZip.Length; i++)
 {
-    if (zipCode != eligibleZip[i])
-    {
-        checkZip = false;
-    }
-    else if (zipCode == eligibleZip[i])
+    // If zip code is in the array set checkZip to true
+    if (zipCode == eligibleZip[i])
     {
         checkZip = true;
-    }
+        break;
+    } // Otherwise checkZip is already set to false
 }
 
+    // If checkZip is true then package is eligible for delivery
     if (checkZip == true)
     {
         Console.WriteLine($"Zip code {zipCode} IS ELIGIBLE for delivery");
-    }
+    } // Else package is not eligible for delivery
     else
     {
         Console.WriteLine($"Zip code {zipCode} IS NOT ELIGIBLE for delivery");
