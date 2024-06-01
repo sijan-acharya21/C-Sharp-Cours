cffe7c9a414fee3672f64b397abb112ef94c992a
sijan-acharya21 <136575897+sijan-acharya21@users.noreply.github.com>
2024-06-01 12:55:01 +1000

    Update Multidimensional_Arrays.cs

--- Section4/Multidimensional_Arrays.cs
+++ Section4/Multidimensional_Arrays.cs
@@ -5,5 +5,36 @@ int[] myArray = { 5, 6, 7 };
 
 // Rectangular array is structured like a rectangular structure
 
- 
-// 2.56 Video
+// Creating a multidimensional array
+
+int[,] myArray = new int[3, 4];
+myArray[0, 0] = 1;
+myArray[0, 1] = 2;
+myArray[0, 2] = 3;
+myArray[0, 3] = 4;
+
+myArray[1, 0] = 1;
+myArray[1, 1] = 2;
+myArray[1, 2] = 3;
+myArray[1, 3] = 4;
+
+myArray[2, 0] = 1;
+myArray[2, 1] = 2;
+myArray[2, 2] = 3;
+myArray[2, 3] = 4;
+
+// 1 2 3 4
+// 1 2 3 4
+// 1 2 3 4
+
+// Short hand for the array above
+int[,] myArrayDuplicate = { { 1, 2, 3, 4 }, { 1, 2, 3, 4 }, { 1, 2, 3, 4 } };
+
+for (int i = 0; i < 3; i++)
+{
+    for (int j = 0; j < 4; j++) 
+    {
+        Console.Write(myArrayDuplicate[i,j]);
+    }
+    Console.WriteLine();
+}
