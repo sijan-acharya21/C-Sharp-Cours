5eedfbc8da40f8e87afde83ede4f8c82c870d8f4
sijan-acharya21 <136575897+sijan-acharya21@users.noreply.github.com>
2024-06-03 02:00:58 +1000

    Update Passing_By_Reference_and_Values.cs

--- Section5/Passing_By_Reference_and_Values.cs
+++ Section5/Passing_By_Reference_and_Values.cs
@@ -1,1 +1,15 @@
+// Pass means sending information to a function through it's parameters
 
+// Anything that is a struct is a value type
+// Anything that is a class is a reference type
+
+public static void GiveExtraCredit(int studentGrade) {
+  studentGrade += 3;
+  Console.WriteLine("Student's grade after extra credit inside function {0}", studentsGrade); 
+}
+
+// Place the following information inside Main method
+int student1Grade = 75;
+Console.WriteLine("Student's grade before extra credit {0}", student1Grade);
+GiveExtraCredit(student1Grade);
+Console.WriteLine("Student's grade after extra credit {0}", student1Grade);
