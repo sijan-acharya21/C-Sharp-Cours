baab73a253fc5812c648adc8427004d920c9ca77
sijan-acharya21 <136575897+sijan-acharya21@users.noreply.github.com>
2024-06-03 01:24:30 +1000

    Update Creating_Methods.cs

--- Section5/Creating_Methods.cs
+++ Section5/Creating_Methods.cs
@@ -1,4 +1,14 @@
 // Program starts running from the Main method
 // A method (function) is an action in programming - WriteLine() is a method for the Console library
 
- 
+// Access modifiers - public means anyone can use this method, private means only the class itself can use this method
+// Return type is the result of the method or the answer which is returned
+// Void means there is no return type
+
+// Creating a method synta
+// accessModifier static returnType methodName
+public static void DisplayMessage() {
+ Console.WriteLine("Hello World");
+}
+
+// Parameters are the information the method needs to complete the task
