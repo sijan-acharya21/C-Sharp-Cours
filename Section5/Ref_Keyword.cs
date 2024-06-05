a4f344c93c4a62cb39eea189d5311d63da64d1e0
sijan-acharya21 <136575897+sijan-acharya21@users.noreply.github.com>
2024-06-05 10:36:26 +1000

    Update Ref_Keyword.cs

--- Section5/Ref_Keyword.cs
+++ Section5/Ref_Keyword.cs
@@ -1,7 +1,14 @@
 // When passing by value the variable value is copied into the function
 // When passing by reference the actual value itself in the memory location is called into the function
 
-// ref keyword is used to force the value type to be passed as a reference 
+/* 
+ref keyword is used to force the value type to be passed as a reference (passing as a location)
+ref has to be in 2 places in the function
+- Inside the parentheses when declaring the data type for the parameter (ref int variableName)
+- When calling the function and putting the parameter name (IncreaseByFive(ref variableName)
+*/
+
+// Use ref keyword when the variable already has an initial value otherwise use out keyword
 int number = 10;
 Console.WriteLine("Value of number after BEFORE calling method: {0}", number);
 IncreaseByFive(ref number);
