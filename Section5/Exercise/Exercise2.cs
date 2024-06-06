99423de4b5af488aab09bac656bdba901144b365
sijan-acharya21 <136575897+sijan-acharya21@users.noreply.github.com>
2024-06-06 21:46:43 +1000

    Update Exercise2.cs

--- Section5/Exercise/Exercise2.cs
+++ Section5/Exercise/Exercise2.cs
@@ -1,1 +1,5 @@
-
+/*
+Create a console-based program whose Main() method prompts the user for an integer value and, in turn,
+passes the value to a method that squares the number and to a method that cubes the number.
+The Cube() method should call the Square() method. The Main() method displays the results returned from each of the other methods.
+*/
