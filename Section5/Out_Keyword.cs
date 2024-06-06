11f45fe0ac430d96f0db60a9e916eb09e2a90939
sijan-acharya21 <136575897+sijan-acharya21@users.noreply.github.com>
2024-06-06 15:56:26 +1000

    Update Out_Keyword.cs

--- Section5/Out_Keyword.cs
+++ Section5/Out_Keyword.cs
@@ -1,1 +1,16 @@
+/*
+- Use out keyword if the variable is unassigned
+- out keyword is placed before 
+*/
 
+// Inside Main method
+int englishMark; // Unassigned variable
+MarkModeration(out englishMark); // English mark INSIDE method: 5
+Console.WriteLine("English mark AFTER moderation: {0}", englishMark); // English mark after calling function: 5
+
+// Outside Main method
+public static void MarkModeration(out int mark) {
+  mark = 0; // Have to set a value for out parameter
+  mark += 5;
+  Console.WriteLine("English mark INSIDE METHOD: {0}", mark);
+}
