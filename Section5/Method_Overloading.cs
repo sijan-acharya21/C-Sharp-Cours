d28ed07ae5163182ce878be1d592b8a675c89104
sijan-acharya21 <136575897+sijan-acharya21@users.noreply.github.com>
2024-06-06 21:01:30 +1000

    Update Method_Overloading.cs

--- Section5/Method_Overloading.cs
+++ Section5/Method_Overloading.cs
@@ -1,1 +1,21 @@
+// Method overloading is having different implementations of a function with the same name
 
+// In order to overload the function needs to have the same name
+// Parameters need to be different type of parameter or different number of parameters
+
+// Inside Main method
+
+
+// Outside Main method
+public static void Add(int x, int y) {
+  int answer = x + y;
+  Console.WriteLine(answer);
+}
+
+public static void Add(string x, string y) {
+  string concatenate = x + " " + y;
+  Consoe.WriteLine(concatenate);
+}
+
+public static void Add(double x, double) {
+  double answer
