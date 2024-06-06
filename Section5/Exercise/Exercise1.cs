399b690ee6026558579d34d67eccad8b8ba476e5
sijan-acharya21 <136575897+sijan-acharya21@users.noreply.github.com>
2024-06-06 21:39:17 +1000

    Update Exercise1.cs

--- Section5/Exercise/Exercise1.cs
+++ Section5/Exercise/Exercise1.cs
@@ -1,1 +1,17 @@
+/*
+Create a console-based application whose Main() method asks the user to input an integer and then calls a method named MultiplicationTable,
+which displays the results of multiplying the integer by each of the numbers 2 through 10
+*/
 
+Console.WriteLine("Enter a number to see multiplication table"); 
+int number = int.Parse(Console.ReadLine());
+MultiplicationTable(number);
+
+public static void MultiplicationTable(int number)
+{
+    for (int i = 1; i < 11; i++)
+    {
+        int answer = number * i;
+        Console.WriteLine($"{number} X {i} = {answer}");
+    }
+}
