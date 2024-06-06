3b7bd6580d2dd19270eb146544298d83a7032dc9
sijan-acharya21 <136575897+sijan-acharya21@users.noreply.github.com>
2024-06-06 20:32:38 +1000

    Update Params_Keyword.cs

--- Section5/Params_Keyword.cs
+++ Section5/Params_Keyword.cs
@@ -1,1 +1,15 @@
+// Inside Main method
+int[] myArray = {1,2,3,4,5};
+PrintArray(myArray);
+Console.WriteLine(
 
+
+
+// Outside Main method
+public static void PrintArray(int[] array) {
+  for (int i = 0; i < array.Length; i++) {
+    Console.WriteLine(array[i]);
+  }
+}
+
+// params keyword allows you to enter any amount of argument
